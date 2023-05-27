72b49b1a461c6d8c1f61de5e0735ca0f311db90c
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2023-05-26 23:34:28 -0500

    rename/resolve deps

--- Package.swift
+++ Package.swift
@@ -11,12 +11,13 @@ let package = Package(
         .library(name: "AggregatorApi", targets: ["AggregatorApi"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/feathercms/feather-core", .branch("dev")),
+		.package(path: "../feather-core"),
+//        .package(url: "https://github.com/feathercms/feather-core", .branch("dev")),
         .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.2.3"),
     ],
     targets: [
         .target(name: "AggregatorApi", dependencies: [
-            .product(name: "FeatherCoreApi", package: "feather-core"),
+            .product(name: "ApiController", package: "feather-core"),
         ]),
         .target(name: "AggregatorModule", dependencies: [
             .product(name: "FeatherCore", package: "feather-core"),
